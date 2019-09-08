3f66d5de83e5537489132b4601c22d63420b24e4
Sergey Dyubov <greypks@gmail.com>
2019-09-09 02:27:35 +0600

    set axios base URL

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css'
 import App from './App'
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://mysterious-reef-29460.herokuapp.com/api/v1/'
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
